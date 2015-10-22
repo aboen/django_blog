4ec709d4f08767b673a2b4e2c28fa3f4ce8ae69e
aboen <aboenx@gmail.com>
2015-10-22 13:30:25 +0700

    Added views to create/edit blog post inside the site.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+__author__ = 'sopyan'
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
